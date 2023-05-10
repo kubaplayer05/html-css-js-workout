40a6878f83d6811346015e96c8c9dd6da11864c0
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-05-10 23:00:30 +0200

    create code that changes border color of .box for every 3 seconds

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello from Nerdbord!')
+const box = document.querySelector(".box")
+
+const colors = ["red","green","blue","black"]
+let index = 0
+
+setInterval(() => {
+    box.style.border = `40px solid ${colors[index]}`
+    index > 3 ? index = 0 : index++
+
+}, 3000)
